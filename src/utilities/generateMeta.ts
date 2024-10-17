02e24eb3abd7a8fa0b979738fcceb62612a0b900
christdevtech <michael@christdev.com>
2024-10-17 16:17:41 +0100

    switched to website template

---
+++ src/utilities/generateMeta.ts
@@ -0,0 +1,36 @@
+import type { Metadata } from 'next'
+
+import type { Page, Post } from '../payload-types'
+
+import { mergeOpenGraph } from './mergeOpenGraph'
+
+export const generateMeta = async (args: { doc: Page | Post }): Promise<Metadata> => {
+  const { doc } = args || {}
+
+  const ogImage =
+    typeof doc?.meta?.image === 'object' &&
+    doc.meta.image !== null &&
+    'url' in doc.meta.image &&
+    `${process.env.NEXT_PUBLIC_SERVER_URL}${doc.meta.image.url}`
+
+  const title = doc?.meta?.title
+    ? doc?.meta?.title + ' | Christdev Digital Academy'
+    : 'Christdev Digital Academy'
+
+  return {
+    description: doc?.meta?.description || 'Empowering the next generation of developers',
+    openGraph: mergeOpenGraph({
+      description: doc?.meta?.description || 'Empowering the next generation of developers',
+      images: ogImage
+        ? [
+            {
+              url: ogImage,
+            },
+          ]
+        : undefined,
+      title,
+      url: Array.isArray(doc?.slug) ? doc?.slug.join('/') : '/',
+    }),
+    title,
+  }
+}
